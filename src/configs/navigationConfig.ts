83ddb18d38f0aefb0a7d5a0c6e3b6e7d0ee73452
Phan Thành Công <cong.pt@ots.vn>
2025-07-15 10:04:41 +0700

    update

---
+++ src/configs/navigationConfig.ts
@@ -0,0 +1,33 @@
+import i18n from "@i18n";
+import { FuseNavItemType } from "@fuse/core/FuseNavigation/types/FuseNavItemType";
+import ar from "./navigation-i18n/ar";
+import en from "./navigation-i18n/en";
+import tr from "./navigation-i18n/tr";
+
+i18n.addResourceBundle("en", "navigation", en);
+i18n.addResourceBundle("tr", "navigation", tr);
+i18n.addResourceBundle("ar", "navigation", ar);
+
+/**
+ * The navigationConfig object is an array of navigation items for the Fuse application.
+ */
+const navigationConfig: FuseNavItemType[] = [
+  {
+    id: "example-component",
+    title: "Example",
+    translate: "EXAMPLE",
+    type: "item",
+    icon: "lucide:star",
+    url: "example",
+  },
+  {
+    id: "tanstack-virtual-demo",
+    title: "TanStack Virtual Demo",
+    translate: "TANSTACK_VIRTUAL_DEMO",
+    type: "item",
+    icon: "lucide:list",
+    url: "tanstack-virtual-demo",
+  },
+];
+
+export default navigationConfig;
